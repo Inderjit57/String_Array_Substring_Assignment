9fec3f3cd3eafb6dcda26a26a8bcd0e6f01d8b9c
Inderjit57 <singhinderjit57@yahoo.com>
2022-02-21 21:56:28 -0500

    Completed

--- src/Question1StringArray/City.java
+++ src/Question1StringArray/City.java
@@ -1,3 +1,8 @@
+//Part 1 : Create a String array which stores names of cities using initialization block.
+//Part 2 : Add code to take a city name as input and search that city name is array created in part1 . 
+//IF a match is found, then print “City name found” else print “City not found in the list”
+//Part 3 : Replace a city which starts with ‘T’   with new city name. New city name = “Mississauga”.
+
 package Question1StringArray;
 
 public class City {
@@ -6,7 +11,7 @@ public class City {
 	public String setCity(String city) {
 		for (int i = 0; i < city.length(); i++) {
 			cityName[i] = city;
-			if (cityName[i].equals(city)) {
+			if (cityName[i].equalsIgnoreCase(city)) {
 				System.out.println("City name found");
 				break;
 			}
@@ -17,14 +22,15 @@ public class City {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		City city = new City();
-		
+
 		city.setCity("Toronto");
-		
+
 		for (int i = 0; i < city.cityName.length; i++) {
-			if(city.cityName[i].startsWith("T") ) {
-				System.out.println(city.cityName[i].replace("T", "Mississauga"));
+			if (city.cityName[i].startsWith("T")) {
+				System.out.println(city.cityName[i].replace(city.cityName[i], "Mississauga"));
+
 			}
 		}
-		
+
 	}
 }
